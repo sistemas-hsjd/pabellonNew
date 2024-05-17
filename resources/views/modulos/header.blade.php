ea2de2f131cc70cc17f622d5104f3a881dc2c497
Nelson Serrano Herrera <nserranoherrera@gmail.com>
2024-05-17 15:53:44 -0400

    sidebar link buscar ficha - administracion

--- resources/views/modulos/header.blade.php
+++ resources/views/modulos/header.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="title" content="AdminLTE 4 | Dashboard">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="AdminLTE is a Free Bootstrap 5 Admin Dashboard featuring over 800 components, 10+ plugins and 20 example pages using Vanilla JS.">
+    <meta name="description" content="Pabellón Digital Hospital San Juan de Dios">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@1.13.1/css/OverlayScrollbars.min.css" integrity="sha256-WKijf8KI68sbq8Znd6yMepIuFF0wdWfIt6gk3JWcQfk=" crossorigin="anonymous">
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     {{-- <link rel="stylesheet" href="{{ Vite::asset('node_modules/@fortawesome/fontawesome-free/css/all.css') }}"> --}}
