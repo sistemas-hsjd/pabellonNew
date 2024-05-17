ee6f780e44ec0fb52f8b86dd7354ef6126c6c2b9
Nelson Serrano Herrera <nserranoherrera@gmail.com>
2024-05-17 11:53:38 -0400

    fontawesome

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,7 @@ window.axios = axios;
 import $ from 'jquery';
 window.jQuery = window.$ = $;
 import 'vue-select/dist/vue-select.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
