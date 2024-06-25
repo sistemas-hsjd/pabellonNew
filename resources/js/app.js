6ca3ebddf28816bfd6f10a58bc6e99721c455a9f
Nelson Serrano Herrera <nserranoherrera@gmail.com>
2024-06-25 16:40:32 -0400

    estilos toastr

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 // import 'toastr/build/toastr.min.css';
 import  VueSelect from "vue-select";
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
-import toastr from 'toastr'
+// import toastr from 'toastr'
 import { createRouter, createWebHistory } from 'vue-router';
 import Routes from './routes.js';
 const app = createApp({});
@@ -10,7 +10,7 @@ const router = createRouter({
     routes:Routes,
     history: createWebHistory(),
 });
-app.config.globalProperties.$toastr = toastr
+// app.config.globalProperties.$toastr = toastr
 app.component("v-select", VueSelect)
 app.use(router);
 app.config.warnHandler = function (msg, vm, trace) {
